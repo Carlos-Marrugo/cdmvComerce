22db38074c95be729e49bc9c1c63a13f3c7b660d
Carlos Marrugo <140767174+Carlos-Marrugo@users.noreply.github.com>
2025-03-10 17:28:05 -0500

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,8 +4,8 @@ import { IonicModule } from '@ionic/angular';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
-  declarations: [ProductCardComponent], // Declarar el componente
+  declarations: [ProductCardComponent], 
   imports: [CommonModule, IonicModule],
-  exports: [ProductCardComponent] // Exportar el componente
+  exports: [ProductCardComponent] 
 })
 export class SharedModule {}
